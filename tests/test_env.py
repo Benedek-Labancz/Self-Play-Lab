f573174359a91f8009e7312324b571ad3365ee0f
Benedek-Labancz <benedeklabancz@proton.me>
2025-11-19 13:10:49 +0000

    Updated basic env tests

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,54 +1,75 @@
+import sys
+from pathlib import Path
+
+project_root = Path(__file__).parent.parent
+sys.path.insert(0, str(project_root))
+
+
 import pytest
 import numpy as np
 from src.environments import TwoDims
+from src.enums.game import BoardEnum
 
 
 env2 = TwoDims()
 
 def test_scoring_cases():
-    cases = env2._scoring_cases()
+    cases = env2._scoring_cases
     assert cases.shape == (8, env2.size, 2)
 
+    e = BoardEnum.EMPTY.value
+    p = BoardEnum.O.value
+    q = BoardEnum.X.value
+
     state1 = np.array([
-        [1, 1, 1],
-        [0, 0, 0],
-        [0, 0, 0]
+        [p, p, p],
+        [e, e, e],
+        [e, e, e]
     ])
 
     state2 = np.array([
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 0, 0]
+        [p, e, e],
+        [p, e, e],
+        [p, e, e]
     ])
 
     state3 = np.array([
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1]
+        [p, e, e],
+        [e, p, e],
+        [e, e, p]
     ])
 
     state4 = np.array([
-        [0, 0, 1],
-        [0, 1, 0],
-        [1, 0, 0]
+        [e, e, p],
+        [e, p, e],
+        [p, e, e]
     ])
 
     state5 = np.array([
-        [1, 1, 1],
-        [0, 1, 0],
-        [0, 0, 1]
+        [p, p, p],
+        [e, p, e],
+        [e, e, p]
     ])
 
     state6 = np.array([
-        [1, 1, 1],
-        [0, 1, 0],
-        [1, 0, 1]
+        [p, p, p],
+        [e, p, e],
+        [p, e, p]
     ])
 
-    assert env2._get_score(state1, 1) == 1
-    assert env2._get_score(state2, 1) == 1
-    assert env2._get_score(state3, 1) == 1
-    assert env2._get_score(state4, 1) == 1
+    state7 = np.array([
+        [q, p, p],
+        [q, q, p],
+        [e, e, q]
+    ])
 
-    assert env2._get_score(state5, 1) == 2
-    assert env2._get_score(state6, 1) == 3
+    assert env2.get_score(state1, p) == 1
+    assert env2.get_score(state2, p) == 1
+    assert env2.get_score(state3, p) == 1
+    assert env2.get_score(state4, p) == 1
+
+    assert env2.get_score(state5, p) == 2
+    assert env2.get_score(state6, p) == 3
+
+    assert env2.get_score(state7, p) == 0
+    assert env2.get_score(state7, q) == 1
