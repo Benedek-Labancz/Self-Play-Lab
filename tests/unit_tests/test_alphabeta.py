29b0b8baf322e52e710871e329e7106b402e0227
Benedek-Labancz <benedeklabancz@proton.me>
2025-12-30 10:34:01 +0100

    Add unit tests

---
+++ tests/unit_tests/test_alphabeta.py
@@ -0,0 +1,82 @@
+import sys
+from pathlib import Path
+
+project_root = Path(__file__).parent.parent.parent
+sys.path.insert(0, str(project_root))
+
+
+import numpy as np
+from typing import Any
+from tqdm import tqdm
+
+from src.agents import MinimaxAgent, AlphaBetaMinimaxAgent
+from src.enums.game import BoardEnum
+from src.environments import TwoDims, ThreeDims
+import pandas as pd
+
+def generate_board_position(env: Any) -> np.ndarray:
+    '''
+    Generate a random position for any 4CE game.
+    This position does not adhere the rules of the game,
+    e.g there might be more Xs than Os.
+    '''
+    bs = env.get_board_state()
+    allowed_values = [BoardEnum.EMPTY.value, BoardEnum.X.value, BoardEnum.O.value]
+    # These values have to appear at least once in the generated position
+    enforced_values = [BoardEnum.EMPTY.value] # make sure there is a valid move
+    num_squares = np.prod(bs.shape)
+    pos = np.concatenate([
+        enforced_values,
+        np.random.choice(allowed_values, size=num_squares - len(enforced_values))])
+    np.random.shuffle(pos)
+    pos = pos.reshape(bs.shape)
+    return pos
+    
+
+def dummy_observation_from_position(env: Any, position: np.ndarray) -> dict:
+    return {
+            "current_player": 0,
+            "next_player": 1,
+            "board": position,
+            "action_mask": env.get_action_mask(position)
+        }
+
+def test_identical_output():
+    envs = [TwoDims(), ThreeDims()]
+    depths = [1, 2, 3, 4, 5]
+    num_positions = [100, 100, 75, 10, 10]
+    num_nodes_explored = {'minimax': [], 'alphabeta': []}
+    for i, depth in enumerate(depths):
+        minimax = MinimaxAgent(search_depth=depth)
+        alphabeta = AlphaBetaMinimaxAgent(search_depth=depth)
+        for env in envs:
+            positions = [generate_board_position(env) for _ in range(num_positions[i])]
+            for position in tqdm(positions):
+                history = [dummy_observation_from_position(env, position)]
+                mm_action = minimax.choose_action(env, history)
+                ab_action = alphabeta.choose_action(env, history)
+                num_nodes_explored['minimax'].append(minimax.nodes_searched)
+                num_nodes_explored['alphabeta'].append(alphabeta.nodes_searched)
+                assert np.all(mm_action == ab_action)
+
+
+    depth_nodes = {'minimax': {env.__class__.__name__: {d: [] for d in depths} for env in envs}, 'alphabeta': {env.__class__.__name__: {d: [] for d in depths} for env in envs}}
+
+    idx = 0
+    for i, depth in enumerate(depths):
+        for _ in range(num_positions[i]):
+            for env in envs:
+                depth_nodes['minimax'][env.__class__.__name__][depth].append(num_nodes_explored['minimax'][idx])
+                depth_nodes['alphabeta'][env.__class__.__name__][depth].append(num_nodes_explored['alphabeta'][idx])
+                idx += 1
+
+    for env in envs:
+        table_data = {
+            'Depth': depths,
+            'Minimax': [np.mean(depth_nodes['minimax'][env.__class__.__name__][d]) for d in depths],
+            'AlphaBeta': [np.mean(depth_nodes['alphabeta'][env.__class__.__name__][d]) for d in depths]
+        }
+        
+        df = pd.DataFrame(table_data)
+        print(f"\n{env.__class__.__name__}:")
+        print(df.to_string(index=False))
---
+++ tests/unit_tests/test_env.py
@@ -0,0 +1,259 @@
+import sys
+from pathlib import Path
+
+project_root = Path(__file__).parent.parent.parent
+sys.path.insert(0, str(project_root))
+
+
+import pytest
+import numpy as np
+from src.environments.two_dims import TwoDims
+from src.environments.three_dims import ThreeDims
+from src.enums.game import BoardEnum
+
+
+env2 = TwoDims()
+env3 = ThreeDims()
+
+def test_two_dims_scoring_cases():
+    cases = env2._scoring_cases
+    assert cases.shape == (8, env2.size, 2)
+
+    e = BoardEnum.EMPTY.value
+    p = BoardEnum.O.value
+    q = BoardEnum.X.value
+
+    state1 = np.array([
+        [p, p, p],
+        [e, e, e],
+        [e, e, e]
+    ])
+
+    state2 = np.array([
+        [p, e, e],
+        [p, e, e],
+        [p, e, e]
+    ])
+
+    state3 = np.array([
+        [p, e, e],
+        [e, p, e],
+        [e, e, p]
+    ])
+
+    state4 = np.array([
+        [e, e, p],
+        [e, p, e],
+        [p, e, e]
+    ])
+
+    state5 = np.array([
+        [p, p, p],
+        [e, p, e],
+        [e, e, p]
+    ])
+
+    state6 = np.array([
+        [p, p, p],
+        [e, p, e],
+        [p, e, p]
+    ])
+
+    state7 = np.array([
+        [q, p, p],
+        [q, q, p],
+        [e, e, q]
+    ])
+
+    assert env2.get_score(state1, p) == 1
+    assert env2.get_score(state2, p) == 1
+    assert env2.get_score(state3, p) == 1
+    assert env2.get_score(state4, p) == 1
+
+    assert env2.get_score(state5, p) == 2
+    assert env2.get_score(state6, p) == 3
+
+    assert env2.get_score(state7, p) == 0
+    assert env2.get_score(state7, q) == 1
+
+
+def test_three_dims_scoring_cases():
+    cases = env3._scoring_cases
+    assert cases.shape == (49, env3.size, 3)
+
+    e = BoardEnum.EMPTY.value
+    p = BoardEnum.O.value
+    q = BoardEnum.X.value
+
+    state1 = np.array([
+        [
+            [p, e, e],
+            [p, e, e],
+            [p, e, e]
+        ],
+        [
+            [e, e, e],
+            [e, e, e],
+            [e, e, e]
+        ],
+        [
+            [e, e, e],
+            [e, e, e],
+            [e, e, e]
+        ]
+    ])
+
+    state2 = np.array([
+        [
+            [p, e, e],
+            [e, p, e],
+            [e, e, p]
+        ],
+        [
+            [e, e, e],
+            [e, e, e],
+            [e, e, e]
+        ],
+        [
+            [e, e, e],
+            [e, e, e],
+            [e, e, e]
+        ]
+    ])
+
+    state3 = np.array([
+        [
+            [p, p, p],
+            [e, e, e],
+            [e, e, e]
+        ],
+        [
+            [e, e, e],
+            [e, e, e],
+            [e, e, e]
+        ],
+        [
+            [e, e, e],
+            [e, e, e],
+            [e, e, e]
+        ]
+    ])
+
+    state4 = np.array([
+        [
+            [p, e, e],
+            [e, e, e],
+            [e, e, e]
+        ],
+        [
+            [p, e, e],
+            [e, e, e],
+            [e, e, e]
+        ],
+        [
+            [p, e, e],
+            [e, e, e],
+            [e, e, e]
+        ]
+    ])
+
+    state5 = np.array([
+        [
+            [p, e, e],
+            [e, e, e],
+            [e, e, e]
+        ],
+        [
+            [e, e, e],
+            [p, e, e],
+            [e, e, e]
+        ],
+        [
+            [e, e, e],
+            [e, e, e],
+            [p, e, e]
+        ]
+    ])
+
+
+    state6 = np.array([
+        [
+            [p, e, e],
+            [e, e, e],
+            [e, e, e]
+        ],
+        [
+            [e, p, e],
+            [e, e, e],
+            [e, e, e]
+        ],
+        [
+            [e, e, p],
+            [e, e, e],
+            [e, e, e]
+        ]
+    ])
+
+    state7 = np.array([
+        [
+            [p, e, e],
+            [e, e, e],
+            [e, e, e]
+        ],
+        [
+            [e, e, e],
+            [e, p, e],
+            [e, e, e]
+        ],
+        [
+            [e, e, e],
+            [e, e, e],
+            [e, e, p]
+        ]
+    ])
+
+    state8 = np.array([
+        [
+            [p, p, p],
+            [p, p, p],
+            [p, p, p]
+        ],
+        [
+            [p, p, p],
+            [p, p, p],
+            [p, p, p]
+        ],
+        [
+            [p, p, p],
+            [p, p, p],
+            [p, p, p]
+        ]
+    ])
+
+    state9 = np.array([
+        [
+            [e, p, p],
+            [p, e, p],
+            [p, p, e]
+        ],
+        [
+            [p, e, p],
+            [e, e, e],
+            [p, e, p]
+        ],
+        [
+            [e, e, e],
+            [e, e, e],
+            [e, e, e]
+        ]
+    ])
+
+    assert env3.get_score(state1, p) == 1
+    assert env3.get_score(state2, p) == 1
+    assert env3.get_score(state3, p) == 1
+    assert env3.get_score(state4, p) == 1
+    assert env3.get_score(state5, p) == 1
+    assert env3.get_score(state6, p) == 1
+    assert env3.get_score(state7, p) == 1
+    assert env3.get_score(state8, p) == 49
+    assert env3.get_score(state9, p) == 0
